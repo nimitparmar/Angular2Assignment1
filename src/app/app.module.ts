cbdbd29c3f451fa1ef328e072e972cdb4021bbd4
nimitparmar <nimit.00786@gmail.com>
2017-05-28 20:12:00 +0530

    angular 4 first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ src/app/main.ts
@@ -0,0 +1,6 @@
+import '../polifills';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
