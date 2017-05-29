e2c8af9289f4ad3c51355d35c406a3a9083b2367
Nimit <nimit.00786@gmail.com>
2017-05-29 15:17:15 +0530

    second angular commit

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
+
+import { UsersRoutingModule } from './users-routing.module';
+import { UsersComponent } from './users.component';
+
+@NgModule({
+    declarations: [
+        UsersComponent
+    ],
+    providers: [],
+    bootstrap: [],
+    imports: [
+        CommonModule,
+        UsersRoutingModule
+    ]
+})
+
+export class UsersModule {
+
+}
